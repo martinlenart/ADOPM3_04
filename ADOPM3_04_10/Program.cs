037b0af367ebbb15672bddac571fbf30fae95a2a
Martin <martin_lenart@icloud.com>
2023-12-29 14:59:18 +0100

    update

--- ADOPM3_04_10/Program.cs
+++ ADOPM3_04_10/Program.cs
@@ -19,27 +19,27 @@ namespace ADOPM3_04_10
         {
             //Dictionary of cars
             Dictionary<string, csCar> dicRegNr= new Dictionary<string, csCar>();
-            dicRegNr.Add("NMN854", new csCar { Year = 2022, Make = "Volvo" });
-            dicRegNr.Add("YMY789", new csCar { Year = 2019, Make = "Suzuki" });
+            dicRegNr.Add("NMN 854", new csCar { Year = 2022, Make = "Volvo" });
+            dicRegNr.Add("YMY 789", new csCar { Year = 2019, Make = "Suzuki" });
             
             //Accessing the Dictionary
-            Console.WriteLine(dicRegNr["NMN854"].Make);
-            csCar myCar = dicRegNr["YMY789"];
+            Console.WriteLine(dicRegNr["NMN 854"].Make);
+            csCar myCar = dicRegNr["YMY 789"];
             Console.WriteLine(myCar);
 
             //Dictionary uses HashCode for the key
-            Console.WriteLine("NMN854".GetHashCode());
-            Console.WriteLine("YMY789".GetHashCode());
+            Console.WriteLine("NMN 854".GetHashCode());
+            Console.WriteLine("YMY 789".GetHashCode());
 
 
             //Value in Dictionary can be of any type, also a List
             Dictionary<string, List<string>> dicFavoriteBands = new Dictionary<string, List<string>>();
 
-            dicFavoriteBands.Add("ACDC", new List<string>() { "Fly on the Wall", "TnT", "For those about to Rock" });
-            dicFavoriteBands.Add("PinkFloyd", new List<string>() { "Dark side of the moon", "The Wall", "Final Cut" });
-            dicFavoriteBands.Add("Bob Dylan", new List<string>() { "Infidels" });
+            dicFavoriteBands.Add("AC/DC", new List<string>() { "Fly on the Wall", "TnT", "For those about to Rock" });
+            dicFavoriteBands.Add("Pink Floyd", new List<string>() { "Dark side of the moon", "The Wall", "Final Cut" });
+            dicFavoriteBands.Add("Bob Dylan", new List<string>() { "Infidels", "Slow Train Comming" });
 
-            var l = dicFavoriteBands["ACDC"];
+            var l = dicFavoriteBands["AC/DC"];
             l.ForEach(item => Console.WriteLine(item));
 
 
